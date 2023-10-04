334a2f0ee6c41cc64f6bf24ef83d18ac077eb5e3
ZarathustraSama <mr.gabriel@live.it>
2023-10-04 09:58:29 +0200

    Changed graph creation, nodes now can allow linked list behaviour

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,25 +1,26 @@
 require 'pry-byebug'
 
+NONE = 'NONE'.freeze
+
 class GameBoard
-  attr_accessor :board, :knight_graph
+  attr_accessor :board
 
   def initialize
     @board = [0, 1, 2, 3, 4, 5, 6, 7].product([0, 1, 2, 3, 4, 5, 6, 7])
   end
 
-  def build_graph(position, path = [position])
-    return nil unless @board.include?(position)
-    return nil if path.count(position) > 1
+  def build_graph(position)
+    return NONE unless @board.include?(position)
 
     root = Node.new(position)
-    root.tl1 = build_graph([position[0] - 1, position[1] + 2], path << [position[0] - 1, position[1] + 2])
-    root.tl2 = build_graph([position[0] - 2, position[1] + 1], path << [position[0] - 2, position[1] + 1])
-    root.tr1 = build_graph([position[0] + 1, position[1] + 2], path << [position[0] + 1, position[1] + 2])
-    root.tr2 = build_graph([position[0] + 2, position[1] + 1], path << [position[0] + 2, position[1] + 1])
-    root.bl1 = build_graph([position[0] - 1, position[1] - 2], path << [position[0] - 1, position[1] - 2])
-    root.bl2 = build_graph([position[0] - 2, position[1] - 1], path << [position[0] - 2, position[1] - 1])
-    root.br1 = build_graph([position[0] + 1, position[1] - 2], path << [position[0] + 1, position[1] - 2])
-    root.br2 = build_graph([position[0] + 2, position[1] - 1], path << [position[0] + 2, position[1] - 1])
+    root.tl1 = build_graph([position[0] - 1, position[1] + 2]) unless root.tl1
+    root.tl2 = build_graph([position[0] - 2, position[1] + 1]) unless root.tl2
+    root.tr1 = build_graph([position[0] + 1, position[1] + 2]) unless root.tr1
+    root.tr2 = build_graph([position[0] + 2, position[1] + 1]) unless root.tr2
+    root.bl1 = build_graph([position[0] - 1, position[1] - 2]) unless root.bl1
+    root.bl2 = build_graph([position[0] - 2, position[1] - 1]) unless root.bl2
+    root.br1 = build_graph([position[0] + 1, position[1] - 2]) unless root.br1
+    root.br2 = build_graph([position[0] + 2, position[1] - 1]) unless root.br2
     root
   end
 end
@@ -29,22 +30,54 @@ class Knight
     board = GameBoard.new
     root = board.build_graph(start)
     queue = [root]
-    path = []
     until queue.empty?
       current = queue.last
-      path << current.position unless current.nil?
-      return print_path(path) if current.position == finish
+      return print_path(create_path(current)) if current.position == finish
 
-      queue.unshift(current.tl1) unless current.tl1.nil? || path.include?(current.tl1)
-      queue.unshift(current.tl2) unless current.tl2.nil? || path.include?(current.tl2)
-      queue.unshift(current.tr1) unless current.tr1.nil? || path.include?(current.tr1)
-      queue.unshift(current.tr2) unless current.tr2.nil? || path.include?(current.tr2)
-      queue.unshift(current.bl1) unless current.bl1.nil? || path.include?(current.bl1)
-      queue.unshift(current.bl2) unless current.bl2.nil? || path.include?(current.bl2)
-      queue.unshift(current.br1) unless current.br1.nil? || path.include?(current.br1)
-      queue.unshift(current.br2) unless current.br2.nil? || path.include?(current.br2)
+      unless current.none?(tl1)
+        current.tl1.parent = current
+        queue.unshift(current.tl1)
+      end
+      unless current.none?(tl2)
+        current.tl2.parent = current
+        queue.unshift(current.tl2)
+      end
+      unless current.none?(tr1)
+        current.tr1.parent = current
+        queue.unshift(current.tr1)
+      end
+      unless current.none?(tr2)
+        current.tr2.parent = current
+        queue.unshift(current.tr2)
+      end
+      unless current.none?(bl1)
+        current.bl1.parent = current
+        queue.unshift(current.bl1)
+      end
+      unless current.none?(bl2)
+        current.bl2.parent = current
+        queue.unshift(current.bl2)
+      end
+      unless current.none?(br1)
+        current.br1.parent = current
+        queue.unshift(current.br1)
+      end
+      unless current.none?(br2)
+        current.br2.parent = current
+        queue.unshift(current.br2)
+      end
       queue.pop
     end
+  end
+
+  def create_path(endpoint)
+    path = [endpoint.position]
+    current = endpoint
+    until current.nil?
+      current = current.parent
+      path << current.position
+    end
+    path
   end
 
   def print_path(path)
@@ -52,15 +85,16 @@ class Knight
     path.each do |x, y|
       puts "[#{x}, #{y}]"
     end
+    puts ''
   end
 end
 
-# The vertex where the position of the knight is connected to the top/bottom left/right tiles(t/b l/r)
 class Node
-  attr_accessor :position, :tl1, :tl2, :tr1, :tr2, :bl1, :bl2, :br1, :br2
+  attr_accessor :position, :parent, :tl1, :tl2, :tr1, :tr2, :bl1, :bl2, :br1, :br2
 
   def initialize(position)
     @position = position
+    @parent = nil
     @tl1 = nil
     @tl2 = nil
     @tr1 = nil
@@ -70,4 +104,12 @@ class Node
     @br1 = nil
     @br2 = nil
   end
+
+  def none?(child)
+    child == NONE
+  end
 end
+
+k = Knight.new
+
+k.knight_moves([0, 0], [3, 3])
